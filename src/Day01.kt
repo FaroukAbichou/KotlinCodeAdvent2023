9141cc1a9f1c7c45847adf76d011b8678e24c412
Farouk-abichou <117727520+Farouk-abichou@users.noreply.github.com>
2023-12-01 11:14:23 +0100

    - Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,21 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+
+    getFinalResult(input).println()
+}
+
+fun getFinalResult(input: List<String>): Int {
+    return input.map {
+        getFirstLastNumber(getNumbers(it))
+    }.sum()
+}
+
+fun getFirstLastNumber(input: String): Int {
+    val firstNumber = input.first().toString()
+    val secondNumber = input.last().toString()
+    return (firstNumber + secondNumber).toInt()
+}
+
+fun getNumbers(input: String): String {
+    return input.filter { it.isDigit() }
 }
