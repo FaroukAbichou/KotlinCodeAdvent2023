adf8ec77b608a9a4b0fe3061475e70be5a5a5ea7
Farouk-abichou <117727520+Farouk-abichou@users.noreply.github.com>
2023-12-01 21:13:01 +0100

    - Day 1 Done

--- src/Day01.kt
+++ src/Day01.kt
@@ -4,17 +4,39 @@ fun main() {
 }
 
 fun getFinalResult(input: List<String>): Int {
-    return input.map {
-        getFirstLastNumber(getNumbers(it))
-    }.sum()
+    return input.sumOf { line ->
+        getLineSum(getSpelledLettersNumber(line))
+    }
 }
 
-fun getFirstLastNumber(input: String): Int {
-    val firstNumber = input.first().toString()
-    val secondNumber = input.last().toString()
-    return (firstNumber + secondNumber).toInt()
+fun getLineSum(pair: List<Int>): Int {
+    return (pair.first().toString().plus(pair.last())).toInt()
 }
 
-fun getNumbers(input: String): String {
-    return input.filter { it.isDigit() }
+fun getSpelledLettersNumber(input: String): List<Int> {
+    val spelledOutNumbers = mapOf(
+        1 to "one",
+        2 to "two",
+        3 to "three",
+        4 to "four",
+        5 to "five",
+        6 to "six",
+        7 to "seven",
+        8 to "eight",
+        9 to "nine"
+    )
+    val result = emptyList<Int>().toMutableList()
+
+    repeat(input.length) { times ->
+        spelledOutNumbers.forEach { (key, value) ->
+            if (input.subSequence(times, input.length).startsWith(value)) {
+                result += key
+            }
+        }
+        if (input[times].isDigit()) {
+            result += input[times].toString().toInt()
+        }
+    }
+
+    return result
 }
