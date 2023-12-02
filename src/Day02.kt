b496c207c95746e5dca603d9f92abe2c09bc9bba
Farouk-abichou <117727520+Farouk-abichou@users.noreply.github.com>
2023-12-02 16:53:44 +0100

    - Day 2 part1

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,35 +1,62 @@
 fun main() {
     val input = readInput("Day02")
-    var IDs = 0
-    input.forEachIndexed { index, game ->
-
-        val parts = getGameParts(game)
-        println(parts)
-        println(isGameValid(parts))
-        if (isGameValid(parts)) IDs += index +1
-        println(IDs)
-    }
-
+    part1(input)
+    part2(input)
 }
 
+fun part2(input: List<String>) {
+    var sumOfPower = 0
+    input.forEachIndexed { index, game ->
+        val parts = getGameParts(game)
+        var redCount = 0
+        var blueCount = 0
+        var greenCount = 0
+        parts.forEach { part ->
+            if (getCubesCountByName("red", part) > redCount) {
+                redCount = getCubesCountByName("red", part)
+            }
+            if (getCubesCountByName("blue", part) > blueCount) {
+                blueCount = getCubesCountByName("blue", part)
+            }
+            if (getCubesCountByName("green", part) > greenCount) {
+                greenCount = getCubesCountByName("green", part)
+            }
+        }
+        sumOfPower += redCount * blueCount * greenCount
+        println(parts)
+        println(redCount)
+        println(blueCount)
+        println(greenCount)
+        println(sumOfPower)
+    }
+//    println(sumOfPower)
+}
+
+fun part1(input: List<String>) {
+    var IDs = 0
+    input.forEachIndexed { index, game ->
+        val parts = getGameParts(game)
+        if (isGameValid(parts)) IDs += index + 1
+    }
+}
+
+
 fun isGameValid(game: List<String>): Boolean {
+    val bagCubes = mapOf(
+        12 to "red",
+        13 to "green",
+        14 to "blue"
+    )
     var isValid = true
+
     game.forEach { part ->
         bagCubes.forEach { (key, value) ->
-            if (part.contains(value) && getCubesCountByName(value, part) > key)  isValid = false
+            if (part.contains(value) && getCubesCountByName(value, part) > key) isValid = false
         }
-
     }
     return isValid
 }
 
-private val bagCubes = mapOf(
-    12 to "red",
-    13 to "green",
-    14 to "blue"
-)
-
-// 9 red, 2 green, 13 blue
 private fun getCubesCountByName(cubeName: String, gamePart: String): Int {
     val cubes = gamePart.split(",")
     var result = 0
@@ -48,14 +75,7 @@ private fun getColorAppearance(cube: String): Int {
     return result.toInt()
 }
 
-
 private fun getGameParts(input: String): List<String> {
     val games = input.substring(input.indexOf(":") + 1)
     return games.split(";")
-}
-
-private fun countMatches(string: String, pattern: String): Int {
-    return string.split(pattern)
-        .dropLastWhile { it.isEmpty() }
-        .toTypedArray().size - 1
 }
